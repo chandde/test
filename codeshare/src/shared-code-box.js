eca1cc7bb3be1205fcd02ee992b405ccf2b341d7
Chandler Deng <chandde@microsoft.com>
2020-05-19 23:41:46 -0700

    resize

--- codeshare/src/shared-code-box.js
+++ codeshare/src/shared-code-box.js
@@ -59,27 +59,27 @@ export class SharedCodeBox extends React.Component {
   }
 
   componentDidMount() {
-    window.addEventListener('resize', _.debounce(this.handleResize.bind(this), 50));
+    // window.addEventListener('resize', _.debounce(this.handleResize.bind(this), 50));
   }
   
-  calculateDiff(prev, current) {
-    const length = Math.min(prev.length, current.length);
-    let left = 0;
-    for(; left < length.length; ++left) {
-      if(prev[left] == current[left]) {
-        continue;
-      }
-    }
+  // calculateDiff(prev, current) {
+  //   const length = Math.min(prev.length, current.length);
+  //   let left = 0;
+  //   for(; left < length.length; ++left) {
+  //     if(prev[left] == current[left]) {
+  //       continue;
+  //     }
+  //   }
 
-    let right = length - 1;
-    for(; right >= left; --right) {
-      if(prev[right] == current[right + current.length - prev.length]) {
-        continue;
-      }
-    }
+  //   let right = length - 1;
+  //   for(; right >= left; --right) {
+  //     if(prev[right] == current[right + current.length - prev.length]) {
+  //       continue;
+  //     }
+  //   }
 
-    return longer.substring(left, right - left + 1);
-  }
+  //   return longer.substring(left, right - left + 1);
+  // }
 
   onInput(e) {
     // const current = e.target.value;
@@ -128,7 +128,7 @@ export class SharedCodeBox extends React.Component {
       </div>
       <textarea
         className='inputTextArea' // {`inputTextArea ${this.state.style}`}
-        onInput={_.debounce(this.onInput.bind(this), 1000)}
+        onInput={_.debounce(this.onInput.bind(this), 200)}
         // onKeyPress={this.onKeyPress.bind(this)}
         value={this.state.text}
         id='textbox'
