c1fa11f1c9bbb7e99646274bd51e1b5d5e333ff7
Chandler Deng <chandde@microsoft.com>
2020-06-26 17:38:52 -0700

    handle custom domain

--- uiserver/server.js
+++ uiserver/server.js
@@ -1,47 +1,28 @@
 const express = require('express');
 const proxy = require('express-request-proxy');
+const newProxy = require('express-http-proxy');
 const app = express();
 
-app.use(
-    "/pages/:l1/:l2/:l3/:l4",
-    proxy({
-        cache: false,
-        url: 'https://bingadssmartpagetest.z19.web.core.windows.net/:l1/:l2/:l3/:l4',
-    })
-);
+// const SERVER = 'bingadssmartpagetest1.centralus.cloudapp.azure.com';
+const CDN = "https://bingadssmartpagetest.azureedge.net/"
 
-app.use(
-    "/pages/:l1/:l2/:l3",
-    proxy({
-        cache: false,
-        url: 'https://bingadssmartpagetest.z19.web.core.windows.net/:l1/:l2/:l3',
-    })
-);
+const customDomainMap = {
+    "www.test.com": "/server1",
+    "www.chandlerdeng.com": "/server1",
+    'bingadssmartpagetest1.centralus.cloudapp.azure.com': '', // for the site itself, do not add ex
+};
 
-app.use(
-    "/pages/:l1/:l2",
-    proxy({
-        cache: false,
-        url: 'https://bingadssmartpagetest.z19.web.core.windows.net/:l1/:l2',
-    })
-);
+function mapDomain (req) {
+    console.log(req.originalUrl); // /123?x=1&y=2
+    console.log(req._parsedUrl.search); // ?x=1&y=2
+    const customDomain = customDomainMap[req.headers.host];
+    console.log(customDomain);
+    const redirectPath = customDomain + req.originalUrl;
+    console.log(redirectPath);
+    return redirectPath;
+}
+app.use('/*', newProxy(CDN, {
+    proxyReqPathResolver: mapDomain
+}));
 
-app.use(
-    "/pages/:l1",
-    proxy({
-        cache: false,
-        url: 'https://bingadssmartpagetest.z19.web.core.windows.net/:l1',
-    })
-);
-
-const greeting = 'Welcome to Microsoft Ads Smart Page! It seems you do not have a smart page created. Please create a new account or sign into https://ads.microsoft.com/ to start!';
-
-app.use('/', function (req, res) {
-    res.send(greeting);
-});
-
-app.use('/home', function (req, res) {
-    res.send(greeting);
-});
-
-app.listen(process.env.PORT || 8080, function () {});
+app.listen(process.env.PORT || 80, function () { });
