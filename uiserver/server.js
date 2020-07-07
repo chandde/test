ebc59ba98d79c21dc6bb52bf4813a4390a4136f9
Chandler Deng <chandde@microsoft.com>
2020-07-06 21:39:38 -0700

    async

--- uiserver/server.js
+++ uiserver/server.js
@@ -51,8 +51,25 @@ async function mapDomain(req) {
 app.use(cors());
 
 app.use('/*', newProxy(CDN, {
-    proxyReqPathResolver: function(req) {
-        return mapDomain(req);
+    proxyReqPathResolver: function (req) {
+        return new Promise((resolve, reject) => {
+            var mapSource = "https://bingadssmartpagetest2.azureedge.net/common/custom-domain-mapping.json";
+            request(mapSource, (error, response, body) => {
+                if (response.statusCode === 200) {
+                    console.log(`original mapping downloaded: ${body}`);
+                    var mapping = JSON.parse(body);                
+                    console.log(req.originalUrl); // /123?x=1&y=2
+                    console.log(req._parsedUrl.search); // ?x=1&y=2
+                    const customDomain = mapping[req.headers.host];
+                    console.log(customDomain);
+                    const redirectPath = (customDomain || '') + req.originalUrl;
+                    console.log(`redirectPath = ${redirectPath}`);
+                    resolve(redirectPath);
+                } else {
+                    reject();
+                }
+            });
+        });
     }
 }));
 
