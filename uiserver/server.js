e3552d51d3a47adc2c18f91b19402adb9bc15f82
Chandler Deng <chandde@microsoft.com>
2020-07-06 21:29:23 -0700

    use mapping from blob

--- uiserver/server.js
+++ uiserver/server.js
@@ -5,21 +5,43 @@ const cors = require('cors');
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
+const request = require('request');
 
 const app = express();
 
 // const SERVER = 'bingadssmartpagetest1.centralus.cloudapp.azure.com';
-const CDN = "https://bingadssmartpagetest2.azureedge.net/"
+const CDN = "https://bingadssmartpagetest2.azureedge.net/";
 
-const customDomainMap = {
-    "www.chandlerdeng.com": "/site1",
-    'bingadssmartpagetest2.centralus.cloudapp.azure.com': '', // for the site itself, do not add ex
-};
+const HOST = 'bingadssmartpagetest2.centralus.cloudapp.azure.com';
 
-function mapDomain (req) {
+// const customDomainMap = {
+//     "www.chandlerdeng.com": "/site1",
+//     'bingadssmartpagetest2.centralus.cloudapp.azure.com': '', // for the site itself, do not add ex
+// };
+
+function mapDomain(req) {
+    if (req.headers.host === HOST) {
+        return '';
+    }
+
+    var mapSource = "https://bingadssmartpagetest2.azureedge.net/common/custom-domain-mapping.json";
+    var mappingStr = await new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (response.statusCode === 200) {
+                resolve(body);
+            } else {
+                reject();
+            }
+        });
+    });
+
+    console.log(`original mapping downloaded: ${mappingStr}`);
+
+    var mapping = JSON.parse(mappingStr);
+
     console.log(req.originalUrl); // /123?x=1&y=2
     console.log(req._parsedUrl.search); // ?x=1&y=2
-    const customDomain = customDomainMap[req.headers.host];
+    const customDomain = mapping[req.headers.host];
     console.log(customDomain);
     const redirectPath = (customDomain || '') + req.originalUrl;
     console.log(redirectPath);
@@ -36,8 +58,8 @@ app.use('/*', newProxy(CDN, {
 var httpServer = http.createServer(app);
 httpServer.listen(80);
 
-var privateKey  = fs.readFileSync('./my.key', 'utf8');
+var privateKey = fs.readFileSync('./my.key', 'utf8');
 var certificate = fs.readFileSync('./my.crt', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+var credentials = { key: privateKey, cert: certificate };
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(443);
