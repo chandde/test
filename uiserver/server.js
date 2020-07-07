1d44caf7b51fbc1b25d9cd9bfb2a8eed11c81ca2
Chandler Deng <chandde@microsoft.com>
2020-07-06 19:18:38 -0700

    use local main.js

--- uiserver/server.js
+++ uiserver/server.js
@@ -8,17 +8,17 @@ const app = express();
 const CDN = "https://bingadssmartpagetest2.z19.web.core.windows.net/"
 
 const customDomainMap = {
-    "www.chandlerdeng.com": "/server1/index.html",
+    "www.chandlerdeng.com": "/site1",
     'bingadssmartpagetest1.centralus.cloudapp.azure.com': '', // for the site itself, do not add ex
 };
 
 function mapDomain (req) {
-    // console.log(req.originalUrl); // /123?x=1&y=2
-    // console.log(req._parsedUrl.search); // ?x=1&y=2
+    console.log(req.originalUrl); // /123?x=1&y=2
+    console.log(req._parsedUrl.search); // ?x=1&y=2
     const customDomain = customDomainMap[req.headers.host];
-    // console.log(customDomain);
+    console.log(customDomain);
     const redirectPath = (customDomain || '') + req.originalUrl;
-    // console.log(redirectPath);
+    console.log(redirectPath);
     return redirectPath;
 }
 app.use('/*', newProxy(CDN, {
