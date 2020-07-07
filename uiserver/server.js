df2ef58213031abfe689ac57ed236d62f38d2044
Chandler Deng <chandde@microsoft.com>
2020-07-06 23:39:41 -0700

    handle HOST

--- uiserver/server.js
+++ uiserver/server.js
@@ -14,61 +14,31 @@ const CDN = "https://bingadssmartpagetest2.azureedge.net/";
 
 const HOST = 'bingadssmartpagetest2.centralus.cloudapp.azure.com';
 
-// const customDomainMap = {
-//     "www.chandlerdeng.com": "/site1",
-//     'bingadssmartpagetest2.centralus.cloudapp.azure.com': '', // for the site itself, do not add ex
-// };
-
-async function mapDomain(req) {
-    if (req.headers.host === HOST) {
-        return '';
-    }
-
-    var mapSource = "https://bingadssmartpagetest2.azureedge.net/common/custom-domain-mapping.json";
-    var mappingStr = await new Promise((resolve, reject) => {
-        request(mapSource, (error, response, body) => {
-            if (response.statusCode === 200) {
-                resolve(body);
-            } else {
-                reject();
-            }
-        });
-    });
-
-    console.log(`original mapping downloaded: ${mappingStr}`);
-
-    var mapping = JSON.parse(mappingStr);
-
-    console.log(req.originalUrl); // /123?x=1&y=2
-    console.log(req._parsedUrl.search); // ?x=1&y=2
-    const customDomain = mapping[req.headers.host];
-    console.log(customDomain);
-    const redirectPath = (customDomain || '') + req.originalUrl;
-    console.log(`redirectPath = ${redirectPath}`);
-    return redirectPath;
-}
-
 app.use(cors());
 
 app.use('/*', newProxy(CDN, {
     proxyReqPathResolver: function (req) {
         return new Promise((resolve, reject) => {
-            var mapSource = "https://bingadssmartpagetest2.azureedge.net/common/custom-domain-mapping.json";
-            request(mapSource, (error, response, body) => {
-                if (response.statusCode === 200) {
-                    console.log(`original mapping downloaded: ${body}`);
-                    var mapping = JSON.parse(body);                
-                    console.log(`req.originalUrl: ${req.originalUrl}`); // /123?x=1&y=2
-                    // console.log(req._parsedUrl.search); // ?x=1&y=2
-                    const customDomain = mapping[req.headers.host];
-                    console.log(`customDomain: ${customDomain}`);
-                    const redirectPath = (customDomain || '') + req.originalUrl;
-                    console.log(`redirectPath = ${redirectPath}`);
-                    resolve(redirectPath);
-                } else {
-                    reject();
-                }
-            });
+            if (req.headers.host === HOST) {
+                resolve(req.originalUrl);
+            } else {
+                var mapSource = "https://bingadssmartpagetest2.azureedge.net/common/custom-domain-mapping.json";
+                request(mapSource, (error, response, body) => {
+                    if (response.statusCode === 200) {
+                        console.log(`original mapping downloaded: ${body}`);
+                        var mapping = JSON.parse(body);
+                        console.log(`req.originalUrl: ${req.originalUrl}`); // /123?x=1&y=2
+                        // console.log(req._parsedUrl.search); // ?x=1&y=2
+                        const customDomain = mapping[req.headers.host];
+                        console.log(`customDomain: ${customDomain}`);
+                        const redirectPath = (customDomain || '') + req.originalUrl;
+                        console.log(`redirectPath = ${redirectPath}`);
+                        resolve(redirectPath);
+                    } else {
+                        reject();
+                    }
+                });
+            }
         });
     }
 }));
