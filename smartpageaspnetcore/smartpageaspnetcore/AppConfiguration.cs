2406c743a6205d60935e1311b836ef9d269cd422
Chandler Deng <chandde@microsoft.com>
2020-09-03 15:30:00 -0700

    split subdomain into new class

---
+++ smartpageaspnetcore/smartpageaspnetcore/AppConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartPageServer
+{
+    public class AppConfiguration
+    {
+        public string Cdn { get; set; }
+        public string Blob { get; set; }
+        public string WwwDomain { get; set; }
+        public string ShortDomain { get; set; }
+        public List<string> AppServiceDomains { get; set; }
+    }
+}
---
+++ smartpageaspnetcore/smartpageaspnetcore/Cache.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartPageServer
+{
+    public class Cache
+    {
+        public string CommonVersion { get; set; }
+        public string IndexHtml { get; set; }
+        public Dictionary<string /*page*/, string /*version*/> PageVersion { get; set; }
+    }
+}
