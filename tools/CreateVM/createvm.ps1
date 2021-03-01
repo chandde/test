cd4ed94ff3b2b3a68989275960642edf9dfa36fc
Chandler Deng <chandde@microsoft.com>
2021-02-28 18:59:45 -0800

    ImportVM

--- tools/CreateVM/createvm.ps1
+++ tools/CreateVM/createvm.ps1
@@ -23,5 +23,34 @@ function CreateVM
     Start-VM $VMName
 }
 
+function ImportVM
+{
+    param([string]$NewVMPath, [string]$OldVMPath)
+
+    $switches = Get-VMSwitch
+    foreach($i in $switches)
+    {
+        if ($i.SwitchType -ceq "External")
+        {
+            $ExternalSwitchName = $i.Name
+        }
+    }
+
+    $vmcx = Get-ChildItem $OldVMPath"\*.vmcx" -Recurse
+
+    Write-Host($vmcx)
+
+    $newVM = Import-VM -Path $vmcx.FullName -VhdDestinationPath $NewVMPath -Copy -GenerateNewId -VirtualMachinePath $NewVMPath
+    
+    Add-VMNetworkAdapter -VMName $newVM.Name -SwitchName $ExternalSwitchName
+
+    $importedName = $newVM.Name
+    $changedName = $importedName + $(Get-Date)
+
+    Rename-VM $importedName $changedName
+
+    Start-VM $changedName
+}
+
 # usage
 # CreateVM -VMName test3 -VMRootFolder d:\hyperv -VHDSize 100GB -VMGen 1 -ISOPath D:\ISO\ubuntu-20.04.2-live-server-amd64.iso -CpuCount 4
